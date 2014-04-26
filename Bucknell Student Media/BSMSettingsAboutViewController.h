7df39785b6042c01c1c06f9cdb43d949130b7a0b
Joe Duvall <jsd013@bucknell.edu>
2014-04-26 02:04:25 -0400

    About page is done. Now takes in an RTF file as input.

---
+++ Bucknell Student Media/BSMSettingsAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSMSettingsAboutViewController.h
+//  Bucknell Student Media
+//
+//  Created by Joe Duvall on 4/25/14.
+//  Copyright (c) 2014 Joseph Duvall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSMSettingsAboutViewController : UIViewController
+
+@end
---
+++ Bucknell Student Media/BSMSettingsAboutViewController.m
@@ -0,0 +1,53 @@
+//
+//  BSMSettingsAboutViewController.m
+//  Bucknell Student Media
+//
+//  Created by Joe Duvall on 4/25/14.
+//  Copyright (c) 2014 Joseph Duvall. All rights reserved.
+//
+
+#import "BSMSettingsAboutViewController.h"
+
+@interface BSMSettingsAboutViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *aboutTextView;
+
+@end
+
+@implementation BSMSettingsAboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSAttributedString *attributedString = [[NSAttributedString alloc] initWithFileURL:[[NSBundle mainBundle] URLForResource:@"Acknowledgements" withExtension:@"rtf"] options:nil documentAttributes:nil error:NULL];
+    _aboutTextView.attributedText = attributedString;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
