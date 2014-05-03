16245906d9bc8324ecdb0eae7be892ee35063821
Joe Duvall <jsd013@bucknell.edu>
2014-05-02 22:42:05 -0400

    set up view controllers

---
+++ Bucknell Student Media/BSMBucknellianViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSMBucknellianViewController.h
+//  Bucknell Student Media
+//
+//  Created by Joe Duvall on 5/2/14.
+//  Copyright (c) 2014 Joseph Duvall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSMBucknellianViewController : UIViewController
+
+@end
---
+++ Bucknell Student Media/BSMSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSMSettingsTableViewController.h
+//  Bucknell Student Media
+//
+//  Created by Joe Duvall on 5/2/14.
+//  Copyright (c) 2014 Joseph Duvall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSMSettingsTableViewController : UITableViewController
+
+@end
